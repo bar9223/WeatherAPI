1cd7cc2c7c8dad83a4cfe767a12a622664771a98
bartosz <bartek_krzyz@wp.pl>
2020-07-31 01:03:42 +0200

    MainEntityManager.php added

---
+++ app/src/EntityManagers/MainEntityManager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 10.04.18
+ * Time: 07:07
+ */
+
+namespace App\EntityManagers;
+
+use Doctrine\ORM\Decorator\EntityManagerDecorator;
+
+class MainEntityManager extends EntityManagerDecorator
+{
+
+}
