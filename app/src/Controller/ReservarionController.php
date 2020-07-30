109a5b446e21ce994fa5d68b035e3cd2fec41452
bartosz <bartek_krzyz@wp.pl>
2020-07-31 01:11:10 +0200

    basic api test

--- app/src/Controller/ReservarionController.php
+++ app/src/Controller/ReservarionController.php
@@ -2,18 +2,30 @@
 namespace App\Controller;
 
 
+use App\Entity\Reservations;
+use App\EntityManagers\MainEntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReservarionController extends AbstractController
 {
+    protected $em;
+    public function __construct(MainEntityManager $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function index()
     {
+        $reservation = $this->em->getRepository(Reservations::class)->findBy([
+            'id' => 1
+        ]);
+        var_dump($reservation);
         return $this->render('index.html.twig', [
-            'posts' => 'test',
+            'reservation' => $reservation,
         ]);
     }
 }
